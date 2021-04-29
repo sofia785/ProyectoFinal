8759cb13d0fccc6b4c79c78be02b57083c85ad39
jsegurar <jsegurar@192.168.100.17>
2021-04-29 11:04:03 -0600

    Refresh 5

--- src/problema3/TercerProblema.java
+++ src/problema3/TercerProblema.java
@@ -1,12 +1,17 @@
 /*
-
+ *
  */
 package problema3;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Sofia
  */
 public class TercerProblema {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+    }
 }
